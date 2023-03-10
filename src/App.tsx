9b3b895d227b7deda846c5a44260e0cbe8b14c72
Vladimir <vladimir.yakimov.9811@gmail.com>
2023-03-10 23:11:41 +0300

    3rd lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import {Accordion} from './components/Accordion/Accordion';
+import {Rating} from './components/Rating/Rating';
 
 
 // function declaration
@@ -9,63 +11,26 @@ function App() {
     // обязана вернуть JSX
     return (
         <div>
-            <AppTitle/>
-            <Rating/>
-            <Accordion/>
-            <Rating/>
+            <PageTitle title={'This is App component'}/>
+            <PageTitle title={'My friends'}/>
+            Article 1
+            <Rating value={3}/>
+            <Accordion title={'Menu'}/>
+            <Accordion title={'Users'}/>
+            Article 2
+            <Rating value={0}/>
+            <Rating value={1}/>
+            <Rating value={2}/>
+            <Rating value={3}/>
+            <Rating value={4}/>
+            <Rating value={5}/>
         </div>
     );
 }
 
-function AppTitle() {
-    console.log('AppTitle rendering')
-    return <>This is App component</>
-}
-
-function Rating() {
-    console.log('Rating rendering')
-    return (
-        <>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-        </>
-    )
-}
-
-function Star() {
-    console.log('Star rendering')
-    return (
-        <div>star</div>
-    )
-}
-
-function Accordion() {
-    console.log('Accordion rendering')
-    return (
-        <div>
-            <AccordionTitle/>
-            <AccordionBody/>
-        </div>
-    )
-}
-
-function AccordionTitle() {
-    console.log('AccordionTitle rendering')
-    return <h3>Меню</h3>
-}
-
-function AccordionBody() {
-    console.log('AccordionBody rendering')
-    return (
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-        </ul>
-    )
+function PageTitle(props: any) {
+    console.log('PageTitle rendering')
+    return <h1>{props.title}</h1>
 }
 
 export default App;
