540ba134439ba25162bf43949f7a2f5429d0d5df
Vladimir <vladimir.yakimov.9811@gmail.com>
2023-04-29 18:35:45 +0300

    Files for examples about conrolled inputs

---
+++ src/Input/ControlledCheckbox.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ControlledCheckbox = () => {
+    return (
+        <input type={'checkbox'}/>
+    )
+}
---
+++ src/Input/ControlledSelect.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ControlledSelect = () => {
+    return (
+        <input type={'secect'}/>
+    )
+}
