b3a1aa1296915800ff48f884d4b0baa27a705fce
Vladimir <vladimir.yakimov.9811@gmail.com>
2023-05-07 12:19:35 +0300

    File for examples with useMemo

---
+++ src/components/UseMemo/UseMemo.tsx
@@ -0,0 +1,21 @@
+import {useState} from 'react';
+
+const UseMemo = () => {
+
+    const [a, setA] = useState<number>(0)
+    const [b, setB] = useState<number>(0)
+
+    return (
+        <>
+            <input value={a} onChange={(e) => setA(+e.currentTarget.value)}/>
+            <input value={b} onChange={(e) => setB(+e.currentTarget.value)}/>
+            <hr/>
+            <div>
+                Result for a: {a}
+            </div>
+            <div>
+                Result for b: {b}
+            </div>
+        </>
+    )
+}
