c85fd1fa30d245e8c0f5eb529087d5d820d4469a
Vladimir <vladimir.yakimov.9811@gmail.com>
2023-06-05 06:35:49 +0300

    examples of side effects in useEffects callback

--- src/components/UseEffect/UseEffect.tsx
+++ src/components/UseEffect/UseEffect.tsx
@@ -3,11 +3,17 @@ import React, {useEffect, useState} from 'react';
 
 export const UseEffect = () => {
 
+    console.log('UseEffect Component')
+
     const [counter, setCounter] = useState(1)
 
     useEffect(() => {
         console.log('UseEffect')
-
+        // document.title = ''
+        // api.getUsers().then('')
+        // setInterval
+        // indexedDB
+        // document.getElementById()
     })
 
     return (
