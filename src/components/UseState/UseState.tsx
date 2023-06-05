8b478a92ed57211fb7a19a2a5452e9df69959d6e
Vladimir <vladimir.yakimov.9811@gmail.com>
2023-06-05 06:03:47 +0300

    Optimization useState with useMemo

--- src/components/UseState/UseState.tsx
+++ src/components/UseState/UseState.tsx
@@ -1,15 +1,24 @@
-import React, {useState} from 'react';
+import React, {useMemo, useState} from 'react';
 
+const generateData = (): number => {
+    console.log('generateData')
+    // difficult counting
+    return 459048593045930
+}
 
 export const UseState = () => {
     console.log('UseState')
 
-    const [counter, setCounter] = useState(0)
+    const initValue = useMemo(generateData, [])
 
+    const [counter, setCounter] = useState<number>(initValue)
 
     return (
         <>
-            <button onClick={() => {setCounter(counter+1)}}>+</button>
+            <button onClick={() => {
+                setCounter(counter + 1)
+            }}>+
+            </button>
             {counter}
         </>
     )
