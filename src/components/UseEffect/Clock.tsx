bec126ac3fac7de44eba941a53fa0965e7639148
Vladimir <vladimir.yakimov.9811@gmail.com>
2023-06-10 20:46:48 +0300

    selfmade analog clock with toggling between analog and digital clocks

--- src/components/UseEffect/Clock.tsx
+++ src/components/UseEffect/Clock.tsx
@@ -1,11 +1,17 @@
 import React, {useEffect, useState} from 'react';
 import {clearInterval} from 'timers';
+import '../../App.css';
 
-const add0 = (time: number) => time < 10? '0' + time: time
+const add0 = (time: number) => time < 10 ? '0' + time : time
 
 export const Clock = () => {
 
     const [clock, setClock] = useState(new Date())
+    const [analog, setAnalog] = useState(false)
+    const type = analog ? 'digital' : 'analog'
+    const onChangeHandler = () => {
+        setAnalog(!analog)
+    }
 
     useEffect(() => {
         const intervalId = setInterval(() => {
@@ -23,9 +29,40 @@ export const Clock = () => {
     const minutes = add0(clock.getMinutes())
     const seconds = add0(clock.getSeconds())
 
+    const hoursForAnalogClock = clock.getHours() * 30
+    const minutesForAnalogClock = clock.getMinutes() * 6
+    const secondsForAnalogClock = clock.getSeconds() * 6
+
     return (
         <>
-            {hours}: {minutes}: {seconds}
+            {analog
+                ? <div className="clock">
+                    <div className="hour-hand"
+                         style={{transform: 'rotate(' + hoursForAnalogClock + 'deg)'}}
+                    >
+                        <div className="hour-hand-child"></div>
+                    </div>
+                    <div className="minute-hand"
+                         style={{transform: 'rotate(' + minutesForAnalogClock + 'deg)'}}
+                    >
+                        <div className="minute-hand-child"></div>
+                    </div>
+                    <div className="second-hand"
+                         style={{transform: 'rotate(' + secondsForAnalogClock + 'deg)'}}
+                    >
+                        <div className="second-hand-child"></div>
+                    </div>
+                </div>
+                : <div className='digital'>
+                    {hours}: {minutes}: {seconds}
+                </div>
+            }
+            <button
+                onClick={onChangeHandler}
+                className='button'
+            >
+                to {type} clock
+            </button>
         </>
     )
 }
